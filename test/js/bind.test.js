8dce1b5414ba3175d377222d1758b63d67c80fe2
spursy <582214816@qq.com>
2017-12-09 00:32:35 +0800

    add bind fun

--- test/js/bind.test.js
+++ test/js/bind.test.js
@@ -10,7 +10,16 @@ describe('test bind file', function() {
     it('test keyword this is undefined ', function(){
        expect.equal(bind.get_age_undefined(), undefined);
     })
+
     it('test keyword this is success ', function(){
         expect.equal(bind.get_age_success(), 11);
+    })
+
+    it ('test keyword this through bind fun', function() {
+        expect.equal(bind.get_age_through_bind(), 11);
+    })
+
+    it ('test keyword this through bind fun', function() {
+        expect.equal(bind.calculate_age(), 30);
     })
 })
