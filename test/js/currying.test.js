706f020b81b1f0493c016ff6aace10c391736b80
spursy <582214816@qq.com>
2017-12-09 23:28:32 +0800

    add high order fun and map_reduce fun

---
+++ test/js/currying.test.js
@@ -0,0 +1,18 @@
+/**
+ * created by spursyy at 2017-12-9
+ */
+var currying = require('../../js/currying.js');
+var expect = require('chai').expect();
+
+//test suite
+describe('test bind file', function() {
+    //test case
+    it('test high order fun', function () {
+        expect.equal(currying.high_order_fun(), 30);
+    })
+
+    it('test map_reduce fun', function () {
+        expect.equal(currying.map_reduce_fun(), 300);
+    })
+
+})
