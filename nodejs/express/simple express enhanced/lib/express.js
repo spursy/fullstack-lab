046eb9c5f8dfd5c856c95b3999c7dafc45abd9d4
spursy <582214816@qq.com>
2017-12-19 00:31:09 +0800

    update simple express enhanced demo

--- nodejs/express/simple express enhanced/lib/express.js
+++ nodejs/express/simple express enhanced/lib/express.js
@@ -1,39 +1,38 @@
-const http = require('http');
-const mixin = require('merge-descriptors');
-const layer = require('../layer/layer.js');
+const http = require('http')
+const mixin = require('merge-descriptors')
 const methods = require('methods');
+const Layer = require('./layer/layer.js')
 
 module.exports = function createServer() {
-    const app = function(req, res) {
-        app.handle(req, res);
+    const app = function (req, res) {
+        app.handle(req, res)
     }
-    mixin(app, proto, false);
-    app.init();
-    return app;
+    mixin(app, proto, false)
+    app.init()
+    return app
 }
 
-var proto = Object.create(null);
-proto.listen = function(port) {
-    const server = http.createServer(this);
-    return server.listen.apply(server, arguments);
+const proto = Object.create(null)
+proto.listen = function (port) {
+    const server = http.createServer(this)
+    return server.listen.apply(server, arguments)
 }
 
-proto.init = function() {
-    this.handles = [] 
+proto.init = function () {
+    this.handles = []
 }
 
-proto.handle = function(req, res) {
-    for (let i=0; i < this.handles.length; i++) {
-        const layer = this.handles[i];
-        layer.handle_request(req, res);
+proto.handle = function (req, res) {
+    // 对handles中的函数进行遍历
+    for (let i = 0; i < this.handles.length; i++) {
+        const layer = this.handles[i]
+        layer.handle_request(req, res)
     }
 }
 
 methods.forEach(function(method) {
     proto[method] = function(fn) {
-        console.log(method);
-        console.log(typeof layer);
-        const layer = new layer(method, fn);
-        this.handles.push(layer);
+        const layer = new Layer(method, fn)
+        this.handles.push(layer)
     }
 })
