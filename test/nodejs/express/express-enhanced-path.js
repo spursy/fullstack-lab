fb3d99e7773312ff14d0f7d669e772079d5ba340
spursy <582214816@qq.com>
2017-12-26 19:05:07 +0800

    add test file

---
+++ test/nodejs/express/express-enhanced-path.js
@@ -0,0 +1,17 @@
+const express = require('../../../nodejs/express/express enhanced path/index');
+
+const app = express()
+
+app.get('/foo', function (req, res, next) {
+  res.end('Welcome to GET /foo')
+})
+
+app.get('/bar', function (req, res, next) {
+  res.end('Welcome to GET /bar')
+})
+
+app.post('/foo', function (req, res, next) {
+  res.end('Welcome to POST /foo')
+})
+
+app.listen(3000)
