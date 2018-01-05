a5d66dbd49ea740125dfbae797551d04bcf4d946
spursy <582214816@qq.com>
2018-01-05 17:16:58 +0800

    add express enhanced params file

---
+++ test/nodejs/express/express-enhanced-params.js
@@ -0,0 +1,24 @@
+/*
+  support req.params
+  support app.param api
+ */
+const express = require('../../../nodejs/express/express enhanced params/index')
+const app = express()
+
+app.get('/user/:userId', function (req, res, next) {
+    res.end(`Welcome, the user.id = ${req.params.userId} and the user.name is ${req.user.name}`)
+})
+
+app.param('userId', function (req, res, next, userId, key) {
+    req.user = {
+        id: userId,
+        name: 'foo'
+    }
+    next()
+})
+
+app.get('/article/:title', function (req, res, next) {
+    res.end(`Welcome, the article's title is ${req.params.title}`)
+})
+
+app.listen(3000)
