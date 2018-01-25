e50891ea82ed36f339afb9db28ad01d87483a650
spursy <582214816@qq.com>
2018-01-25 14:26:48 +0800

    add memory leak test fun.

---
+++ memory-leak/leak-index.js
@@ -0,0 +1,20 @@
+'use strict';
+const heapdump = require('heapdump');
+let leakObject = null;
+let count = 0;
+
+setInterval(function testMemoryLeak() {
+    const originLeakObject = leakObject;
+    const unused = function () {
+        if (originLeakObject) {
+            console.log('originLeakObject');
+        }
+    };
+    leakObject = {
+        count: String(count++),
+        leakStr: new Array(1e7).join('*'),
+        leakMethod: function () {
+            console.log('leakMessage');
+        }
+    };
+}, 1000);
