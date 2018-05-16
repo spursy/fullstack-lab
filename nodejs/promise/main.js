9ba47ec16e81c2301affae9264c9746c9952a3d1
spursy <582214816@qq.com>
2018-05-16 23:40:42 +0800

    update

--- nodejs/promise/main.js
+++ nodejs/promise/main.js
@@ -1,34 +1,17 @@
 let main = Object.create(null);
 let Promise = require('bluebird')
-let readFile = Promise.promisify(require("fs").readFile);
+let fs = Promise.promisifyAll(require("fs"));
 const path = require('path');
 
-
 main.getPromises = function() {
-    //return Promise.resolve('the main promises')
-    // fs.readFile('./demo.txt').then((data) => {
-    //     console.log(data);
-    // })
-    return Promise.resolve({
-        then: function(onFulfill, onReject) {
-            readFile("./demo.txt", "utf8").then(function(contents) {
-                console.log(contents);
-                onFulfill(contents);
-            }).catch(function(e) {
-                onReject(e);
-            });
-        }
-    }).then(function(ret) {
-        return ret;
-    }, function(err) {
-        return {};
-    });
-    // readFile("./demo.txt", "utf8").then(function(contents) {
-    //     console.log(contents);
-    //     return Promise.resolve(contents);
-    // }).catch(function(e) {
-    //     console.error(e.stack);
-    // });
+    return new Promise(function(resolve, reject) {
+        fs.readFile("./demo.txt", "utf8").then(function(contents) {
+            console.log(contents);
+            resolve(contents);
+        }).catch(function(e) {
+            reject(e);
+        });
+    })
 }
 
 var GetFirstPromises = function() {
